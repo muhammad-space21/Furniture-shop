9b6d4913d2a4aeba396d8119a1d8e65be9567749
Daler Zokirov <zokirov.d@list.ru>
2020-05-06 02:02:09 +0500

    made the marketing section responsive

--- src/components/IconCards/index.js
+++ src/components/IconCards/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 import './styles.scss';
 
-import { Container, Row, Col } from 'react-bootstrap';
-
 import IconOne from '../../assets/icons/quality.png';
 import IconTwo from '../../assets/icons/shipped.png';
 import IconThree from '../../assets/icons/armchair.png';
@@ -13,64 +11,58 @@ import ButtonLong from '../Buttons/ButtonLong';
 
 
 const IconCards = () => (
-    <Container>
-        <div className='icon-cards'>
-                <div className='marketing-heading'>
-                <h1>
-                Why to choose our products  
-                </h1>
-            </div>
-            <Row>
-                <Col><div className='cards-body'>
-                        <div className='rounded-cards' >
-                            <img src={IconOne} alt="IconOne"/>
-                            <p className='cards-text'>
-                                Lorem ipsum dolor sit amet, 
-                            <br /> 
-                                consectetur adipisicing elit.
-                            </p>
-                        </div>
-                    </div>
-                </Col>
-                <Col><div className='cards-body'>
-                        <div className='rounded-cards' >
-                            <img src={IconTwo} alt="IconTwo"/>
-                            <p className='cards-text'>
-                                Lorem ipsum dolor sit amet, 
-                            <br /> 
-                                consectetur adipisicing elit.
-                            </p>
-                        </div>
-                    </div>
-                </Col>
-                <Col><div className='cards-body'>
-                        <div className='rounded-cards' >
-                            <img src={IconThree} alt="IconThree"/>
-                            <p className='cards-text'>
-                                Lorem ipsum dolor sit amet, 
-                            <br /> 
-                                consectetur adipisicing elit.
-                            </p>
-                        </div>
-                    </div>
-                </Col>
-                <Col><div className='cards-body' id='last-card'>
-                        <div className='rounded-cards' >
-                            <img src={IconFour} alt="IconFour"/>
-                            <p className='cards-text'>
-                                Lorem ipsum dolor sit amet, 
-                            <br /> 
-                                consectetur adipisicing elit.
-                            </p>
-                        </div>
-                    </div>
-                </Col>
-            </Row>
-    </div>
-    <div className='btn-lg-holder'>
-        <ButtonLong />
-    </div>
-    </Container>
+	<>
+		<div className='marketing-heading'>
+				<h1>
+						Why to choose our products  
+				</h1>
+		</div>
+		<div className='icon-cards-container'>
+					<div className='cards'>
+						<div className='rounded-cards' >
+								<img src={IconOne} alt="IconOne"/>
+								<p className='cards-text'>
+										Lorem ipsum dolor sit amet, 
+								<br /> 
+										consectetur adipisicing elit.
+								</p>
+						</div>
+					</div>
+					<div className='cards'>
+						<div className='rounded-cards' >
+								<img src={IconTwo} alt="IconTwo"/>
+								<p className='cards-text'>
+										Lorem ipsum dolor sit amet, 
+								<br /> 
+										consectetur adipisicing elit.
+								</p>
+						</div>
+						</div>
+					<div className='cards'>
+						<div className='rounded-cards' >
+								<img src={IconThree} alt="IconThree"/>
+								<p className='cards-text'>
+										Lorem ipsum dolor sit amet, 
+								<br /> 
+										consectetur adipisicing elit.
+								</p>
+						</div>
+					</div>
+					<div className='cards'>
+						<div className='rounded-cards' >
+								<img src={IconFour} alt="IconFour"/>
+								<p className='cards-text'>
+										Lorem ipsum dolor sit amet, 
+								<br /> 
+										consectetur adipisicing elit.
+								</p>
+						</div>
+					</div>
+		</div>
+		<div className='btn-lg-holder'>
+				<ButtonLong />
+		</div>
+	</>
 );
 
 export default IconCards;
