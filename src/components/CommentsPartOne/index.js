69284cbaf6a7fe28f164613ddd28e5a0cf042542
Daler Zokirov <zokirov.d@list.ru>
2020-05-06 01:14:58 +0500

    made comments section responsive

--- src/components/CommentsPartOne/index.js
+++ src/components/CommentsPartOne/index.js
@@ -2,23 +2,18 @@ import React from 'react';
 
 import './styles.scss';
 
-import { Container, Row, Col } from 'react-bootstrap';
-
 import ArrowRight from '../../assets/images/others/arrow.right.png';
 import ImageCommentOne from '../../assets/images/others/furniture2.jpg';
 
 
 const CommentsPartOne = () => (
-    <div className='comments-part-one'>
-        <Container >
-            <Row>
-                <Col><img className='image-comment-one' src={ImageCommentOne} alt=""/></Col>
-                <Col><p className='comments'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod <br /> tempor incididunt ut labore et dolore magna aliqua. Netus et malesuada <br /> fames ac. </p>
-                    <img src={ArrowRight} className='arrow-right' alt="ArrowRight"/>
-                </Col>
-            </Row>
-        </Container>
-    </div>
+  <div className='comments-part-one'>
+      <img className='image-comment-one' src={ImageCommentOne} alt=""/>
+			<div className='comments'>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod <br /> tempor incididunt ut labore et dolore magna aliqua. Netus et malesuada <br /> fames ac. </p>
+					<img src={ArrowRight} className='arrow-right' alt="ArrowRight"/>
+			</div>
+  </div>
 );
 
 export default CommentsPartOne;
--- src/components/CommentsPartTwo/index.js
+++ src/components/CommentsPartTwo/index.js
@@ -2,23 +2,18 @@ import React from 'react';
 
 import './styles.scss';
 
-import { Container, Row, Col } from 'react-bootstrap';
-
 import ImageCommentTwo from '../../assets/images/others/furniture1.jpg';
 import ArrowLeft from '../../assets/images/others/arrow.right.png';
 
 
 const CommentsPartTwo = () => (
-    <div className='comments-part-two'>
-        <Container>
-            <Row>
-                <Col><p className='comments-text-two'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod <br /> tempor incididunt ut labore et dolore magna aliqua. Netus et malesuada <br /> fames ac. </p>
-                    <img src={ArrowLeft} className='arrow-right' alt="ArrowLeft"/>
-                </Col>
-                <Col> <img className='image-comments-two' src={ImageCommentTwo} alt="ImageCommentTwo"/></Col>
-            </Row>
-        </Container>
-    </div>
+  <div className='comments-part-two'>
+			<div className='comments-text-two'>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod <br /> tempor incididunt ut labore et dolore magna aliqua. Netus et malesuada <br /> fames ac. </p>
+					<img src={ArrowLeft} className='arrow-right' alt="ArrowLeft"/>
+			</div>
+    	<img className='image-comments-two' src={ImageCommentTwo} alt="ImageCommentTwo"/>
+  </div>
 );
 
 export default CommentsPartTwo; 
