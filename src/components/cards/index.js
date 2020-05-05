06daa4b83005d3ef3f63a7e262c08a6d7bc33a73
Daler Zokirov <zokirov.d@list.ru>
2020-05-06 00:47:37 +0500

    made the catalog section responsive

--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 import './styles.scss';
 
-import { Container, Row, Col } from 'react-bootstrap';
 import OrderButton from '../Buttons/OrderButton';
-
 
 import  Image1  from '../../assets/images/catalog/chair2.jpg';
 import  Image2  from '../../assets/images/catalog/chair1.jpg';
@@ -14,79 +12,60 @@ import  Image5  from '../../assets/images/catalog/stool3.jpg';
 import  Image6  from '../../assets/images/catalog/sofa.jpg';
 
 const Cards = () => (
-    <Container>
-        <div className='first-row-cards'>
-            <Row>
-                <Col lg={4} md={4} sm={6} xs={12} >
-                    <div className='cards'>
-                        <div className='image-holder'>
-                            <img className='image' src={Image1} alt='Image1' />
-                        </div>
-                        <div className='footer'>
-                    
-                            <OrderButton />
-                        </div>
-                    </div> 
-                </Col>
-                <Col lg={4} md={4} sm={6} xs={12} >
-                    <div className='cards'>
-                        <div className='image-holder'>
-                            <img className='image' src={Image2} alt='Image2' />
-                        </div>
-                        <div className='footer'>
-            
-                            <OrderButton />
-                        </div>  
-                    </div>  
-                </Col>
-                <Col lg={4} md={4} sm={6} xs={12} >
-                    <div className='cards'>
-                        <div className='image-holder'>
-                            <img className='image' src={Image3} alt='Image3' />
-                        </div>
-                        <div className='footer'>
-                
-                            <OrderButton />
-                        </div>
-                    </div>
-                </Col>
-            </Row>
-        </div>
-        <div className='second-row-cards'>
-            <Row>
-                <Col lg={4} md={4} sm={6} xs={12} >
-                    <div className='cards'>
-                        <div className='image-holder'>
-                            <img className='image' src={Image4} alt='Image4' />
-                        </div>
-                        <div className='footer'>
-                            <OrderButton />
-                        </div>
-                    </div> 
-                </Col>
-                <Col lg={4} md={4} sm={6} xs={12} >
-                    <div className='cards'>
-                        <div className='image-holder'>
-                            <img className='image' src={Image5} alt='Image5' />
-                        </div>
-                        <div className='footer'>
-                            <OrderButton />
-                        </div>  
-                    </div>  
-                </Col>
-                <Col lg={4} md={4} sm={6} xs={12} >
-                    <div className='cards' id='cards-second-row'>
-                        <div className='image-holder'>
-                            <img className='image' src={Image6} alt='Image6' />
-                        </div>
-                        <div className='footer'>
-                            <OrderButton />
-                        </div>
-                    </div>
-                </Col>
-            </Row>
-        </div>
-    </Container>
-)
+  <>
+		<div className='first-row-cards'>
+				<div className='cards'>
+						<div className='image-holder'>
+								<img className='image' src={Image1} alt='Image1' />
+						</div>
+						<div className='footer'>
+								<OrderButton />
+						</div>
+				</div> 
+				<div className='cards'>
+						<div className='image-holder'>
+								<img className='image' src={Image2} alt='Image2' />
+						</div>
+						<div className='footer'>
+								<OrderButton />
+						</div>  
+				</div>  
+				<div className='cards'>
+						<div className='image-holder'>
+								<img className='image' src={Image3} alt='Image3' />
+						</div>
+						<div className='footer'>
+								<OrderButton />
+						</div>
+				</div>
+		</div>
+		<div className='second-row-cards'>
+				<div className='cards'>
+						<div className='image-holder'>
+								<img className='image' src={Image4} alt='Image4' />
+						</div>
+						<div className='footer'>
+								<OrderButton />
+						</div>
+				</div> 
+				<div className='cards'>
+						<div className='image-holder'>
+								<img className='image' src={Image5} alt='Image5' />
+						</div>
+						<div className='footer'>
+								<OrderButton />
+						</div>  
+				</div>  
+				<div className='cards' id='cards-second-row'>
+						<div className='image-holder'>
+								<img className='image' src={Image6} alt='Image6' />
+						</div>
+						<div className='footer'>
+								<OrderButton />
+						</div>
+				</div>
+		</div>
+	</>
+);
 
 export default Cards;
