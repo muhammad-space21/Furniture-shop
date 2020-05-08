364cc816a029ee176efd1d0d5440d334d3d9a9b5
Daler Zokirov <zokirov.d@list.ru>
2020-05-08 13:41:34 +0500

    created errorboundary and spinner components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Switch, Route } from 'react-router-dom';
-
 import './App.css';
-
-import HomePage from './pages/homepage';
-import CommentPage from './pages/CommentsPage';
-import OrderPage from './pages/OrderPage';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import ErrorBoundary from './components/ErrorBoundary';
+import Spinner from './components/Spinner';
+
+const HomePage = lazy(() => import('./pages/homepage'));
+const CommentPage = lazy(() => import('./pages/CommentsPage'));
+const OrderPage = lazy(() => import('./pages/OrderPage'));
 
 class App extends React.Component {
 
@@ -15,9 +16,13 @@ class App extends React.Component {
   return (
     <div className="App">
       <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/comment-page' component={CommentPage} />
-        <Route exact path='/order-page' component={OrderPage} />
+        <ErrorBoundary>
+          <Suspense fallback={<Spinner />}>
+            <Route exact path='/' component={HomePage} />
+            <Route exact path='/comment-page' component={CommentPage} />
+            <Route exact path='/order-page' component={OrderPage} />
+          </Suspense>
+        </ErrorBoundary>
       </Switch>
     </div>
   );
---
+++ src/components/Spinner/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.scss';
+
+const Spinner = ()  => {
+  return (
+    <div className='spinner-overlay'>
+      <div className='spinner-container'/>
+    </div>
+  )
+};
+
+export default Spinner;
